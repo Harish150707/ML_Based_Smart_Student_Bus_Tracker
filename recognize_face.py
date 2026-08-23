11eff484cd50b3110e2ced74234108972b6754ac
Harish T <harishthirusangu1507@gmail.com>
2026-08-23 15:56:13 +0530

    Update recognize_face.py

--- recognize_face.py
+++ recognize_face.py
@@ -17,8 +17,8 @@ app = Flask(__name__)
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME", "harisheyy1507@gmail.com")
-app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD", "tdtnlhlaoladqama")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME", "")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD", "")
 
 mail = Mail(app)
 
